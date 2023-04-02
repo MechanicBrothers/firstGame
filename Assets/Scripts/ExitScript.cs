64d8a5534306b790d6c27ab87bfbf7395657615c
Ernesto Jet <ericge@yandex.ru>
2023-04-02 22:40:32 +0400

    Добавил кнопки

---
+++ Assets/Scripts/ExitScript.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitScript : MonoBehaviour
+{
+  public void ExitFunction()
+    {
+        Application.Quit();
+        Debug.Log("Выход");
+    }
+}
